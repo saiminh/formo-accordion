15dc5d6d7d7fa185cfe51cef6f123531942e94d0
saiminh <saiminh@gmail.com>
2024-09-09 22:28:54 +0200

    version

--- formo-accordion.php
+++ formo-accordion.php
@@ -4,7 +4,7 @@
  * Description:       Accordion Block for FAQs
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.2
  * Author:            forpeople
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
